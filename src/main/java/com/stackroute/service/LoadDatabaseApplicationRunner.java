99d12d904a62333c52fd422f4883c8447a0d1c7b
febin.thomas <febin.thomas@cgi.com>
2019-10-03 14:42:02 +0530

    Adding Application Runner pre filling database

---
+++ src/main/java/com/stackroute/service/LoadDatabaseApplicationRunner.java
@@ -0,0 +1,25 @@
+package com.stackroute.service;
+
+import com.stackroute.domain.Movie;
+import com.stackroute.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoadDatabaseApplicationRunner implements ApplicationRunner {
+    @Autowired
+    private final MovieRepository movieRepository;
+
+    public LoadDatabaseApplicationRunner(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        movieRepository.save(new Movie(245,"IMDB id IronMan",234,"Language","Movie Title:Spidey","Movie Posterpath",
+                1000,120,"12th September 2019","Movie Overview","Movie TagLine"));
+
+    }
+}
