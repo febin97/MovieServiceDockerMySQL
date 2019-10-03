172a7f6b969133e45d28feb9b3f42e2cbcfdf852
febin.thomas <febin.thomas@cgi.com>
2019-10-03 11:38:48 +0530

    Swagger 2

--- src/main/java/com/stackroute/controller/MovieController.java
+++ src/main/java/com/stackroute/controller/MovieController.java
@@ -9,40 +9,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/movies")
 public class MovieController {
     private final MovieService movieService;
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
-    @GetMapping("/movies")
+    @GetMapping("/")
     public List<Movie> GetAllMovies(){
         return movieService.GetAllMovies();
     }
-    @PostMapping("/movies")
+    @PostMapping("/")
     public Movie addNewMovie(@RequestBody Movie newMovie){
         return movieService.AddNewMovie(newMovie);
     }
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public Movie getParticularMovie(@PathVariable int id){
         return movieService.GetParticularMovie(id);
     }
-    @GetMapping("/movies/movieName={movieTitle}")
+    @GetMapping("/movieName={movieTitle}")
     public List<Movie> getMoviesByTitle(@PathVariable String movieTitle){
         return movieService.GetMovieByTitle(movieTitle);
     }
-    @PutMapping("/movies/{id}")
+    @PutMapping("/{id}")
     public Movie updateMovie(@RequestBody Movie newMovie, @PathVariable int id){
         return movieService.UpdateMovie(newMovie,id);
     }
-    @DeleteMapping("/movies/{id}")
+    @DeleteMapping("/{id}")
     public void deleteMovie(@PathVariable int id){
         movieService.DeleteMovie(id);
-    }
-
-    @GetMapping("/")
-    public String Hello(Model model){
-        model.addAttribute("name","Hello World");
-        return "index";
     }
 
 }
